1cc15926301895f7cfd7eac110f396fc40dc9e2d
watanabe-miyuki <kirin.myk2018@gmail.com>
2023-05-07 19:02:17 +0900

    style: :art: userServise を 名前空間に設定

--- front/src/services/userService.js
+++ front/src/services/userService.js
@@ -1,12 +1,14 @@
-export const fetchUsers = async () => {
+const UserService = {
+  fetchUsers: async function () {
     try {
-      const response = await fetch('http://localhost/api/show/user');
+      const response = await fetch("http://localhost/api/show/user");
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
       return [];
     }
-  };
+  },
+};
 
-  
+export { UserService };
