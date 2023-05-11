2eb17ebce5bf46f58e90d79160223dbe28d0c7c1
karen <caremayo812@icloud.com>
2023-05-11 16:35:14 +0900

    feat: :art: モーダル追加

--- front/src/pages/UserForm.js
+++ front/src/pages/UserForm.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 import CardForm from "../components/CardForm.js";
+import Modal from "../components/Modal.js";
 import UserHeader from "../components/UserHeader-simple.js";
 import PropTypes from "prop-types";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import { Box, Typography, Container, TextField, Button, Link } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Container,
+  TextField,
+  Button,
+  Link,
+} from "@mui/material";
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
+const UserForm = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-function UserForm() {
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div className="App">
       <UserHeader></UserHeader>
       <Container>
         <Box className="flex items-center">
-            <Link href="/UserTop">
-              <span><ArrowBackIosIcon /></span>
-            </Link>
-            
-            <Typography variant="h4" component="h1">
-                アイテム出品
-            </Typography>
+          <Link href="/UserTop">
+            <span>
+              <ArrowBackIosIcon />
+            </span>
+          </Link>
+
+          <Typography variant="h4" component="h1">
+            アイテム出品
+          </Typography>
         </Box>
         <Box className="flex justify-center mt-10">
           <Box className="w-6/12">
@@ -54,13 +72,24 @@ function UserForm() {
           <TextField label="" variant="filled" className="w-full" />
         </Box>
         <Box className="flex justify-center">
-        <Link href="/">
-        <Button variant="contained" className="rounded-md bg-teal-400 px-2 py-3 my-2 text-lg">確認画面へ</Button>
-        </Link>
+            <Button
+              variant="contained"
+              className="rounded-md bg-teal-400 px-2 py-3 my-2 text-lg"
+              onClick={openModal}
+            >
+              確認画面へ
+            </Button>
+            <Modal
+              open={isModalOpen}
+              onClose={closeModal}
+              title={"出品しますか？"}
+              cancelButtonText="入力に戻る"
+              confirmButtonText="はい"
+            />
         </Box>
       </Container>
     </div>
   );
-}
+};
 
 export default UserForm;
