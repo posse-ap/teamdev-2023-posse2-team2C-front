fa07a0b6052a00594389a76855cc543f7317a114
karen <caremayo812@icloud.com>
2023-05-22 16:52:02 +0900

    返却画面のフロント移動させてきた

---
+++ front/src/pages/mypage/rentalItems/Top.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import Card from "../../../components/Card.js";
+import UserTab from "../../../components/UserTab.js";
+import UserHeader from "../../../components/UserHeader.js";
+import SelectBox from "../../../components/SelectBox.js";
+import PropTypes from "prop-types";
+import {
+  Box,
+  Typography,
+  Paper,
+  Switch,
+  Container,
+  Grid,
+  dividerClasses,
+  Checkbox,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel
+} from "@mui/material";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+const UserTop=() => {
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost/api/mypage/rentals/1');
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(items);
+  return (
+    <div className="App">
+      <UserHeader></UserHeader>
+      <UserTab></UserTab>
+      <Container>
+        <SelectBox></SelectBox>
+        <Grid container spacing={10}>
+          {items?.map((item) => (
+            <Card event={item} />
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default UserTop;
