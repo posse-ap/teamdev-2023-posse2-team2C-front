49ca92389f4e7f5fb6a62c0e5781587a5197cdac
keishiro terashita <20153180g@gmail.com>
2023-05-28 07:23:01 +0900

    出品審査に権限チェックつけちゃった

--- front/src/pages/admin/ListingReview/index.js
+++ front/src/pages/admin/ListingReview/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
-import ListingReviewTable from '@/components/admin/ListingReviewTable';
+import ListingReviewTable from "@/components/admin/ListingReviewTable";
+import axios from "axios";
 
 const ListingReview = () => {
   const [requests, setRequests] = useState([]);
@@ -33,16 +34,25 @@ const ListingReview = () => {
       setRequests(data);
     };
 
-    fetchData();
+    axios
+      .get("http://localhost:80/api/role", { withCredentials: true })
+      .then((response) => {
+        if (response.data !== 2) {
+          alert("アクセス権限がありません。TOPページに戻ります。");
+          router.push("/UserTop");
+          return;
+        } else {
+          fetchData();
+        }
+      })
+      .catch(function (error) {
+        alert("ログイン情報がありません。");
+        router.push("/auth/login");
+        return;
+      });
   }, []);
 
-  const headers = [
-    "ユーザー名",
-    "商品名",
-    "申請日時",
-    "",
-  ];
-
+  const headers = ["ユーザー名", "商品名", "申請日時", ""];
 
   return (
     <Box sx={{ p: 4 }}>
