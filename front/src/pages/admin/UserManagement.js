c48b67f35b410d8927ef8f7eef6fa1596905b4e9
keishiro terashita <20153180g@gmail.com>
2023-05-27 12:31:21 +0900

    権限チェック

--- front/src/pages/admin/UserManagement.js
+++ front/src/pages/admin/UserManagement.js
@@ -3,18 +3,34 @@ import { Box, Typography } from "@mui/material";
 import ManagementTable from "@/components/admin/UserManagementTable";
 import { UserService } from "@/services/userService";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const UserManagement = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState();
+  const router = useRouter();
+  const fetchData = async () => {
+    const data = await UserService.fetchUsers();
+    setUsers(data);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await UserService.fetchUsers();
-      setUsers(data);
-    };
-
-    fetchData();
+    axios
+    .get("http://localhost:80/api/role", { withCredentials: true })
+    .then((response) => {
+      if (response.data !== 2) {
+        alert("アクセス権限がありません。TOPページに戻ります。");
+        router.push("/UserTop");
+        return;
+      } else {
+        fetchData();
+      }
+    })
+    .catch(function (error) {
+      alert("ログイン情報がありません。");
+      router.push("/auth/login");
+      return;
+    });
   }, []);
 
   const handleClickDeleteButton = async (user_id) => {
@@ -38,19 +54,19 @@ const UserManagement = () => {
     "",
   ];
 
-  return (
-    <Box sx={{ p: 4 }}>
-      <Typography variant="h4" component="h1" gutterBottom sx={{ px: 2 }}>
-        ユーザー一覧
-      </Typography>
-      <ManagementTable
-        data={users}
-        headers={headers}
-        handleClickDeleteButton={handleClickDeleteButton}
-        handleClickRoleButton={handleClickRoleButton}
-      />
-    </Box>
-  );
+    return (
+      <Box sx={{ p: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom sx={{ px: 2 }}>
+          ユーザー一覧
+        </Typography>
+        <ManagementTable
+          data={users}
+          headers={headers}
+          handleClickDeleteButton={handleClickDeleteButton}
+          handleClickRoleButton={handleClickRoleButton}
+        />
+      </Box>
+    );
 };
 
 export default UserManagement;
