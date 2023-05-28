3d251d5fea4509de2533f4535d104b0754d55e9f
keishiro terashita <20153180g@gmail.com>
2023-05-28 18:39:41 +0900

    自分の出品一覧・編集

---
+++ front/src/pages/mypage/list.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import Card from "@/components/Card.js";
+import UserTab from "@/components/UserTab.js";
+import UserHeader from "@/components/UserHeader.js";
+import SelectBox from "@/components/SelectBox.js";
+import PropTypes from "prop-types";
+import {
+  Box,
+  Typography,
+  Paper,
+  Switch,
+  Container,
+  Grid,
+  dividerClasses,
+  Checkbox,
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
+import axios from "axios";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+const cardList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(`http://localhost/api/mypage/myItems`, {
+          withCredentials: true,
+        })
+        .then((response) => {
+          setItems(response.data);
+        });
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(items);
+  return (
+    <div className="App">
+      <Container>
+        <Grid container spacing={10}>
+          {items?.map((item) => (
+            <Card event={item} />
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default cardList;
