192657c48ad17950d1877ed2a0a5546e4be04984
watanabe-miyuki <kirin.myk2018@gmail.com>
2023-05-28 13:49:40 +0900

    feat: :art: wip マイページ各ページへ遷移

--- front/src/components/MyPageHeader.js
+++ front/src/components/MyPageHeader.js
@@ -1,34 +1,65 @@
 import React from "react";
-import { Grid, AppBar, Toolbar, MenuItem, Typography, Box } from "@mui/material";
+import { Grid, AppBar, Toolbar, MenuItem, Typography } from "@mui/material";
 import { UserContext } from "../pages/_app";
+import { useRouter } from "next/router";
 
 const MyPageHeader = () => {
-    const user = React.useContext(UserContext);
+  const user = React.useContext(UserContext);
+  const router = useRouter();
 
-    return (
-      <AppBar position="static" className="bg-blue-50" elevation={0}>
-        <Toolbar>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-            <Typography color={'black'}>
-            {user ? user.name : 'Loading...'}さんのマイページ
-            </Typography>
-            </Grid>
-            <Grid item>
-              <MenuItem sx={{ color: 'black' }}>
-              <Typography textAlign="center">アカウント情報</Typography>
-              </MenuItem>
-            </Grid>
-            <Grid item>
-              <MenuItem sx={{ color: 'black' }}>出品・主催</MenuItem>
-            </Grid>
-            <Grid item>
-              <MenuItem sx={{ color: 'black' }}>レンタル・参加イベント</MenuItem>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    );
+  const onClickNavigation = (path) => {
+    router.push(path);
   };
 
-  export default MyPageHeader;
+  return (
+    <AppBar position="static" className="bg-blue-50" elevation={0}>
+      <Toolbar>
+        <Grid container alignItems="center" spacing={10}>
+          <Grid item xs={4}>
+            <Typography color={"black"}>
+              {user ? user.name : "Loading..."}さんのマイページ
+            </Typography>
+          </Grid>
+          <Grid item>
+            <MenuItem
+              sx={{ color: "black" }}
+              onClick={() => onClickNavigation("/MyPage")}
+              selected={router.pathname === "/MyPage"}
+            >
+              アカウント情報
+            </MenuItem>
+          </Grid>
+          <Grid item>
+            <MenuItem
+              sx={{ color: "black" }}
+              onClick={() => onClickNavigation("/MyPage/list")}
+              selected={router.pathname === "/MyPage/list"}
+            >
+              出品
+            </MenuItem>
+          </Grid>
+          <Grid item>
+            <MenuItem
+              sx={{ color: "black" }}
+              onClick={() => onClickNavigation("/MyPage/rentalItems")}
+              selected={router.pathname === "/MyPage/rentalItems"}
+            >
+              レンタル品
+            </MenuItem>
+          </Grid>
+          <Grid item>
+            <MenuItem
+              sx={{ color: "black" }}
+              onClick={() => onClickNavigation("/MyPage/helpPage")}
+              selected={router.pathname === "/MyPage/helpPage"}
+            >
+              ヘルプ
+            </MenuItem>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MyPageHeader;
