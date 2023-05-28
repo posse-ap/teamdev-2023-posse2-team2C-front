fe5260c174076c3e1f585ab7fb661912ee1d9e7a
karen <caremayo812@icloud.com>
2023-05-28 20:09:51 +0900

    ダミーテキスト削除＋出品アイテムなしの場合の対処

--- front/src/pages/ItemDetail/[item_num].js
+++ front/src/pages/ItemDetail/[item_num].js
@@ -106,7 +106,6 @@ const ItemDetail = () => {
           <Box className="ml-10 mb-5">
             <Typography variant="subtitle" component="p">
               {item.detail}
-              この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、
             </Typography>
           </Box>
           <Typography variant="h6" component="p">
